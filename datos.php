c2d621caa2028b8965054be8c7c800dbb0d36be2
CaballoCatacumbaPosicionada <luis.martinez@insbaixcamp.cat>
2026-01-26 09:09:31 +0100

    Update datos.php

--- datos.php
+++ datos.php
@@ -1,2 +1,9 @@
-print('from action
-print
+
+<from action = "Inicio.php" method = "post">
+    <label for = "user">Escriu el teu nom:</label><br>
+    <input type = "text" id = "user" name = "user"><br>
+   
+    <label for = "cont">Escriu el teu nom:</label><br>
+    <input type = "text" id = "cont" name = "cont"><br>
+
+
