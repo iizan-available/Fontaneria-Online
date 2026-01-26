5317c939e276e6e2ecce5526f7a7fbe614456268
CaballoCatacumbaPosicionada <luis.martinez@insbaixcamp.cat>
2026-01-26 10:13:14 +0100

    Update datos.php

--- datos.php
+++ datos.php
@@ -1,8 +1,9 @@
-<from action = "Inicio.php" method = "post">
-    <label for = "user">Escriu el teu nom:</label><br>
+<from action = "C:\xampp\htdocs\Fontanetia" method = "post">
+    <label for = "user">Introdueix el teu nom d'usuari:</label><br>
     <input type = "text" id = "user" name = "user"><br>
    
-    <label for = "cont">Escriu el teu nom:</label><br>
+    <label for = "cont">Introdueix la teva contraseña:</label><br>
     <input type = "text" id = "cont" name = "cont"><br>
 
-
+    <input type="submit" value = "Submit">
+</from>
