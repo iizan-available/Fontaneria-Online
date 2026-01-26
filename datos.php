bc936de667af4e2bfdbe8b5f0df44bc9370f3c4f
CaballoCatacumbaPosicionada <luis.martinez@insbaixcamp.cat>
2026-01-26 10:28:01 +0100

    Update datos.php

--- datos.php
+++ datos.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-<from action = "C:\xampp\htdocs\Fontanetia\contacto.php" method = "post">
+<from action = "contacto.php" method = "post">
 
     <label for = "user">Introdueix el teu nom d'usuari:</label><br>
     <input type = "text" id = "user" name = "user"><br>
    
-    <label for = "cont">Introdueix la teva contraseña:</label><br>
-    <input type = "text" id = "cont" name = "cont"><br>
+    <label for = "pass">Introdueix la teva contraseña:</label><br>
+    <input type = "text" id = "pass" name = "pass"><br>
 
     <input type="submit" value = "Submit">
 </from>
