dcccaa75c7573d9e09b85d1b386f7c0d68ab302d
iizan-available <izan.arcos@insbaixcamp.cat>
2026-02-02 10:31:01 +0100

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php include('cabecera.php')?>
+<html>
+    <head>
+        <style>
+        </style>
+        <div style="background-color:#6b2a2b; padding: 10px; text-align: center;">
+            <a href="Tu Fontaneria de Confianza" style="color: white; text-decoration: none;">
+                Tu Fontaneria<br>de Confianza
+            </a>
+    </head>
+    <body>
+    <h2>Nuestros Productos:<h2>
+    <?php include('plantillas/paixeta1.php')?>
+    </body>
+
+<?php include('piedepagina.php')?>
+</html>
