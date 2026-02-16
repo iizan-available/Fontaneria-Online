9d6adcbb3c6e2240c51d6d0fb7fe38112bdf05ac
CaballoCatacumbaPosicionada <luis.martinez@insbaixcamp.cat>
2026-02-16 15:18:43 +0100

    tetas augmentadas

---
+++ aixeta2.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<?php include 'cabecera.php'; ?>
+<html>
+    <h1 class="h1">
+        <b>Fregadero bajo encimera Leda Syan</b>
+    </h1>
+    <h4 class="txt">
+        El fregadero Leda de Syan es un fregadero de cocina de montaje bajo encimera (undermount),<p>
+         diseñado para integrarse de forma discreta y elegante bajo la superficie de tu encimera.
+    </h4>
+    <h4 class="price">
+        267€
+    </h4>   
+    <h4 class="desc">
+
+        Es un fregadero de cocina bajo encimera de la línea Leda de la marca Syan, diseñado para integrarse de forma discreta y moderna bajo la encimera de tu cocina.<p>
+
+        📐 Dimensiones y compatibilidad<p>
+
+        Medidas aproximadas: 55,5 cm de ancho x 51,5 cm de fondo.<p>
+
+        Profundidad útil alrededor de 22,5 cm, proporcionando un buen espacio para lavar utensilios y alimentos.<p>
+
+        Recomendado para muebles a partir de 60 cm de ancho.<p>
+
+        🧱 Diseño y montaje<p>
+
+        Tipo: bajo encimera, lo que significa que queda instalado por debajo de la superficie de trabajo para un acabado más limpio y sin bordes visibles.<p>
+    </h4>
+    <body>
+        <img src='img\leda.png' alt="No s'ha pogut carregar l'imatge"
+        class="btn" width=450>
+    </body>
+
+    <a href="errorpag.php" class= a>
+        <button class="button button2">COMPRAR</button> 
+    </a>
+    
+    <style>
+    .price{
+        margin-top: 1;
+        position: relative;
+        left: 560px;
+        font-size: 30px;
+        top: 50px;
+    }
+    .h1{
+        position: relative;
+        top: 50px;
+        left: 560px;
+    }
+    .txt{
+        position: relative;
+        top: 50px;
+        left: 560px;
+    }
+    .btn {
+        text-align: center;
+        position: relative;
+        left: 100px;
+        top: -460px;
+        margin-bottom: -250px;
+    }
+    .a{
+        text-align: center;
+        position: relative;
+        top: -283px;
+        left: 100px;
+    }
+    .desc{
+        text-align: left;
+        position: relative;
+        top: 230px;
+        left: 100px; 
+    }
+
+    </style>
+</html>
+<html>
+    <?php include 'piedepagina.php'; ?>
